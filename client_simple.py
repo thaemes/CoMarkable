02f322d2ffe17d08868cbdf7060f0bfcd43af55a
Lorenzo Gatti <l.gatti@utwente.nl>
2021-02-18 12:12:40 +0100

    Updated client
    - use with for automatic context management of socket & device file
    - quit gracefully (not yet handled by the server, tho)
    
    TODO: turn into nonblocking client using select

--- client_simple.py
+++ client_simple.py
@@ -10,21 +10,19 @@ import socket
 import struct
 
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('143.178.219.194' if len(argv) == 1 else argv[1], 33333))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+	client.connect(('143.178.219.194' if len(argv) == 1 else argv[1], 33333))
 
-
-wacomEvents = open('/dev/input/event0', "rb+", buffering=0)
-
-rec = ""
-
-try:
-	while True:
-		rec = client.recv(16)
-		if len(rec) == 16:
-			wacomEvents.write(rec)
-	
-finally:
-	print(rec)
-	wacomEvents.close()
-
+	with open('/dev/input/event0', "rb+", buffering=0) as wacomEvents:
+		rec = ""
+		try:
+			while True:
+				rec = client.recv(16)
+				if len(rec) == 16:
+					wacomEvents.write(rec)
+		except Exception as e:
+			print(f"Something Bad happened: {e}")
+			print(f"Last received message: {rec}")
+		except KeyboardInterrupt:
+			print("Gracefully quitting...")
+			exit()
