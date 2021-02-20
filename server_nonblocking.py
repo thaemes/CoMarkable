148e45d287d1944da5af7efa2a76fc4987485988
Lorenzo Gatti <l.gatti@utwente.nl>
2021-02-20 12:51:23 +0100

    Nonblocking server using select for every read/write operation
    also introduced a send buffer and provisions for a receive buffer

--- server_nonblocking.py
+++ server_nonblocking.py
@@ -11,46 +11,63 @@ import socket
 import struct
 import select
 import pdb
+import queue
 
 EV_ABS = 3
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind(('0.0.0.0', 33333))
 server.listen()
 server.setblocking(False)
+inputs = [server]
+outputs = []
+send_queue = queue.Queue()
+receive_queue = queue.Queue()
 
-wacomEvents = ''
 
 try:
-	while True:
-		print('Waiting for client...')
-		ready_to_read, ready_to_write, in_error = \
-				   select.select(
-					  [server], #socket to read from
-					  [],
-					  [],
-					  60)
-		if server in ready_to_read:
-			client, addr = server.accept()
-			print('Client connected')
-			# Start reading input from wacom digitizer:
-			wacomEvents = open('/dev/input/event0', 'rb')
-	
-			while True:
-				#check whether we can read data from pen and/or socket
-				ready_to_read, ready_to_write, in_error = \
-				   select.select(
-					  [wacomEvents,server], #socket to read from (tablet & client
-					  [server],
-					  [],
-					  60)
-				#if we have data from the pen, read it
-				if wacomEvents in ready_to_read:
-					local_pen_data = wacomEvents.read(16)
+	print('Waiting for client...')
+	with open('/dev/input/event0', 'rb') as digitizer:
+		inputs.append(digitizer)
+		while True:
+
+			ready_to_read, ready_to_write, in_error = \
+					   select.select(
+						  inputs, #socket to read from
+						  outputs,
+						  [],
+						  60)
+			for obj in ready_to_read:
+				#we can connect to the client
+				if obj is server:
+					connection, client_address = server.accept()
+					print(f'Client connected: {client_address} {connection.fileno()}')
+					connection.setblocking(False)
+					inputs.append(connection)
+					outputs.append(connection)
+				#we can read something from the client
+				elif isinstance(obj,socket.socket):
+					#todo: write this data we are receiving to digitizer
+					print('received remote data (unhandled)')
+				#we can read from our pen and add it to the send buffer
+				elif obj is digitizer:
+					local_pen_data = digitizer.read(16)
+					send_queue.put(local_pen_data)
+
+			for obj in ready_to_write:
+				#we can send data
+				if isinstance(obj,socket.socket):
+					while not send_queue.empty():
+						pen_data = send_queue.get_nowait()
+						obj.send(pen_data)
+				#we can write data to the digitizer
+				elif obj is digitizer:
+					while not receive_queue.empty():
+						pen_data = send_queue.get_nowait()
+						#write pen data to the digitizer
 					
-					#doesn't work yet, BUT might be because the client is in blocking mode?
-					#if server in ready_to_write:
-					client.send(local_pen_data) 
+			for obj in in_error:
+				print(f"Error status with {obj}")
 finally:
-	wacomEvents.close()
 	server.close()
